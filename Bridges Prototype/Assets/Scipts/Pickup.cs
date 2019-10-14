1806f03e5653627e0c05b787d34b2c0b8a04e5f1
ArtfulRoger23 <47542733+ArtfulRoger23@users.noreply.github.com>
2019-10-15 00:33:32 +0100

    First Push
    First push of project

---
+++ Bridges Prototype/Assets/Scipts/Pickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    public Transform endTrans;
+
+    [SerializeField]
+    float interpolation;
+
+    float timer = 0;
+
+
+    void Update()
+    {
+        if (Input.GetKey("f"))
+        {
+            
+            GetComponent<Rigidbody>().useGravity = false;
+
+            timer += Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, endTrans.position, timer * interpolation);
+
+            //GameObject.Find("Destination").GetComponent<Transform>().position
+        }
+
+
+    }
+}
+
+
